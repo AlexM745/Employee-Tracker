a1d59f677ad933e24a0759741cdf39a3293a8496
Alexis Merino <alexismerino49@gmail.com>
2024-04-14 15:02:11 -0500

    added starting code for the schema, such as creating the database and the tables needed

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+-- if the database exist then do not create it 
+DROP DATABASE IF EXISTS  employeeTracker_db; 
+
+-- creates a database if it does not exist
+CREATE DATABASE employeeTracker_db;
+
+-- switches to the employeeTracker database
+\c employeeTracker_db
+
+CREATE TABLE department()
+
+CREATE TABLE employee()
+
+CREATE TABLE role ()
