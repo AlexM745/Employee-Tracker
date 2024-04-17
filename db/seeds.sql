44a6655fb74c7745b65b1b90659690ed5b9678b4
Alexis Merino <alexismerino49@gmail.com>
2024-04-16 20:35:03 -0500

    added the names, roles and managers of employees and comments

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
+--  these are all the departments by name
 INSERT INTO department (department_name)
 VALUES
 ("Production"), 
@@ -10,6 +11,7 @@ VALUES
 ("Programming"), 
 ("Quality Assurance")
 
+--  all the roles and the salaries and the id to which department the role is part of
 INSERT INTO role (title, salary, department_id)
 VALUES
 ("Lead Game Producer", 120000, 1 ),
@@ -35,6 +37,27 @@ VALUES
 ("Quality Assurance Build Engineer", 110000, 8)
 
 
-
+-- this are all the employees with there role id and who is thier manager
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
+("Bill","Fellows", 1, 2),
+("Jason","Haller", 2, NULL), 
+("John","Santana", 3, 1), 
+("Robert","Smith", 4, 3), 
+("Hester","Johnson", 5, 3),
+("Miles","Morales", 6, 3),
+("Fred","Cox", 7, 2),
+("Peter","Parker", 8, 1),
+("Tony","Stark", 9, 1),
+("Steve","Rogers", 10, 3), 
+("Alex","Garcia", 11, 3), 
+("Mary","Miller", 12, 3), 
+("Lexi","Davis", 13, 3), 
+("James","Williams", 14, 1), 
+("Sarah","Lee", 15, 1), 
+("Claire","Thompson", 16, 15), 
+("Bob","Thomas", 17, 15), 
+("Jill","Smallwood", 18, 1 ), 
+("Jane","Harris", 19, 18),  
+("Victoria","White", 20, 21), 
+("Sierra","Martin", 21, 1), 
