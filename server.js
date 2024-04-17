e4b4f4bd34aac38d15fb926cbdab582dbb2f9682
Alexis Merino <alexismerino49@gmail.com>
2024-04-17 00:22:18 -0500

    added addDepartment and figlet for the the terminal art. Also added the console.tables

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
+// importing inquirer
 const inquirer = require("inquirer");
-const db = require("./db");
+const figlet = require('figlet');
 
 //  creating a connection pool 
 const { Pool } = require('pg');
@@ -9,26 +10,32 @@ require('dotenv').config();
 
 // uses the environment variables to connect to database
 const pool = new Pool(
-
     {
-        database: "employeetracker_db",
-        password: process.env.DB_PASSWORD,
+        host: 'localhost',
         user: process.env.DB_USER,
-        host: "localhost",
+        password: process.env.DB_PASSWORD,
+        database: 'employeetracker_db',
     },
-
-    console.log('Connected to the employeetracker_db database.')
 )
 // calling pool to use the environment vatiables to start the connection to database
 pool.connect((error) => {
     if (error) throw error;
-    console.log("Did not connect!");
+    console.log('Connected to the employeetracker_db database.')
     beginning();
 });
 
 
 //initial question and options that the user will be provided with 
 beginning = () => {
+    console.log(figlet.textSync("Employee Tracker", {
+        font: "Standard",
+        horizontalLayout: "default",
+        verticalLayout: "default",
+        width: 80,
+        whitespaceBreak: true,
+    })
+    );
+
     inquirer.prompt([
 
         {
@@ -71,7 +78,7 @@ beginning = () => {
                     break;
                 // this exit selection will end the connection with SQL and will display the following message on the terminal
                 case "Exit":
-                    connection.end();
+                    pool.end();
                     console.log(" You have logged off the Employee tracker!\n")
                     return;
                 // if no choice is made then the console will 
@@ -83,26 +90,52 @@ beginning = () => {
         })
 }
 
-viewAllEmployees = () => {
-    pool.query(
-    `SELECT employee.employee_id, employee.first_name, employee.last_name, roles.title, department.department_name, roles.salary, CONCAT(manager.first_name, '', manager.last_name) manager 
+viewEmployees = () => {
+    pool.query(`
+    SELECT employee.employee_id, employee.first_name, employee.last_name, roles.title, department.department_name, roles.salary, CONCAT(manager.first_name, '', manager.last_name) manager 
     FROM employee 
     JOIN  employee.employee ON employee.manager_id = manager.employee_id 
     JOIN roles ON employee.roles_id = roles.roles_id 
     JOIN department ON department.department_id = roles.department_id 
-    ORDER BY employee.employee_id ASC;`, (error, res) => {
+    ORDER BY employee.employee_id`, (error, res) => {
         if (error) throw (error);
+        console.table(res);
         beginning();
     })
 }
 
-viewAllDepartments = () => {
-    pool.query(`SELECT * 
-    FROM department 
-    ORDER BY department_id ASC;`, (error, res) => {
+viewDepartments = () => {
+    pool.query(`SELECT * FROM department ORDER BY id`, (error, res) => {
         if (error) throw (error);
+        console.table(res);
         beginning();
     })
+}
+
+addDepartment = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "department",
+            message: "What is the new departments name?",
+            validate: deparmentInput => {
+                if (deparmentInput) {
+                    return true;
+                } else {
+                    console.log("Add a department");
+                    return false;
+                }
+            }
+        }])
+        .then((answer) => {
+            pool.query(`INSERT INTO department (name) VALUES(?)`, [answer.deparment], (error, res) => {
+
+                if (error) throw (error);
+                console.table(res);
+                console.log(`Department ${answer.deparment} was added.`);
+                beginning();
+            })
+        });
 }
 
 
