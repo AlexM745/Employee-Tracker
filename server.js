e31f34e7b504125f15172464a5907615773558e6
Alexis Merino <alexismerino49@gmail.com>
2024-04-18 21:01:13 -0500

    added the add employee function

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ pool.connect((error) => {
 
 //initial question and options that the user will be provided with 
 beginning = () => {
-    
+
 
     inquirer.prompt([
 
@@ -112,6 +112,58 @@ viewDepartments = () => {
         beginning();
     })
 }
+// this function adds a new employee
+addEmployee = () => {
+    pool.query(`SELECT * FROM roles;`, (error, res) => {
+        if (error) throw error;
+        let roles = res.map(roles => ({name: roles.title, value: roles.id }));
+        pool.query(`SELECT * FROM employee;`, (error, res) => {
+            if (error) throw error;
+            let employees = res.map(employee => ({ name: employee.first_name + " " + employee.last_name, value: employee.id }));
+
+            inquirer.prompt([
+                //  the first name of new employee
+                {
+                    type: "input",
+                    name: "firstname",
+                    message: "What is the first name of the new employee?",
+
+                },
+                // last name of the new employee
+                {
+                    type: "input",
+                    name: "lastname",
+                    message: "What is the last name of the new employee?",
+
+                },
+                // the role title that the employee will have
+                {
+                    type: "rawlist",
+                    name: "role",
+                    message: "What is the title of the new employee?",
+                    choices: roles
+                },
+                // the manager of the employee
+                {
+                    type: "rawlist",
+                    name: "manager",
+                    message: "Who is the manager of the new employee?",
+                    choices: employees
+
+                }])
+                .then((answers) => {
+                    pool.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES(?,?,?,?)`, [answers.firstname, answers.lastname, answers.roles, answers.manager.id], (error, res) => {
+
+                        if (error) throw (error);
+                        console.log(`Added ${answers.firstname} ${answers.lastname} to database.`);
+                        beginning();
+                    })
+                });
+
+        })
+    })
+}
+
 
 
 
@@ -131,7 +183,7 @@ addDepartment = () => {
             }
         }])
         .then((answer) => {
-            pool.query(`INSERT INTO department (name) VALUES(?)`, [answer.newDeparment], (error, res) => {
+            pool.query(`INSERT INTO department (department_name) VALUES(?)`, [answer.newDeparment], (error, res) => {
 
                 if (error) throw (error);
                 console.log(`${answer.deparment} department was added to database.`);
